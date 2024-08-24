344c47590287047506a7ce612f4ce55a3a811f5b
lain62 <laimadelain@gmail.com>
2024-08-24 14:10:32 +0700

    added project layers

--- lib/drogmo/drogmo.rb
+++ lib/drogmo/drogmo.rb
@@ -26,7 +26,6 @@ module Drogmo
     [components[1..-1], components[0]]
   end
 
-  #
   # Main class for parsing the project file (ex: project.ogmo)
   # Class must be created before creating the level class
   class Project
@@ -77,10 +76,77 @@ module Drogmo
       end
     end
   end
-  require_relative 'project_layer_tile'
-  require_relative 'project_layer_entity'
-  require_relative 'project_layer_grid'
-  require_relative 'project_layer_decal'
+
+  # Class for a layer tile in a project file
+  class ProjectLayerTile
+    attr_reader :definition, :name, :grid_size, :export_id, :export_mode, :array_mode, :default_tileset
+
+    # @param data_raw [Hash] raw data of layer
+    def initialize(data_raw)
+      @definition = data_raw['definition']
+      @name = data_raw['name']
+      @grid_size = [data_raw['gridSize']['x'], data_raw['gridSize']['y']]
+      @export_id = data_raw['exportID']
+      @export_mode = data_raw['exportMode']
+      @array_mode = data_raw['arrayMode']
+      @default_tileset = data_raw['defaultTileset']
+    end
+  end
+
+  # Class for entity layer in a project file
+  class ProjectLayerEntity
+    attr_reader :definition, :name, :grid_size, :export_id, :required_tags, :excluded_tags
+
+    # @param data_raw [Hash] raw data of layer
+    def initialize(data_raw)
+      @definition = data_raw['definition']
+      @name = data_raw['name']
+      @grid_size = [data_raw['gridSize']['x'], data_raw['gridSize']['y']]
+      @export_id = data_raw['exportID']
+      @required_tags = data_raw['requiredTags']
+      @excluded_tags = data_raw['excludedTags']
+    end
+  end
+
+  # Class for grid layer in a project file
+  class ProjectLayerGrid
+    attr_reader :definition, :name, :grid_size, :export_id, :array_mode, :legend, :legendRGBA
+
+    # @param data_raw [Hash] raw data of layer
+    def initialize(data_raw)
+      @definition = data_raw['definition']
+      @name = data_raw['name']
+      @grid_size = [data_raw['gridSize']['x'], data_raw['gridSize']['y']]
+      @export_id = data_raw['exportID']
+      @array_mode = data_raw['arrayMode']
+      @legend = data_raw['legend']
+      @legendRGBA = {}
+      @legend.each do |attribute|
+        color_convert = attribute[1].gsub('#', '')
+        @legendRGBA[attribute[0]] = Drogmo.color(color_convert)
+      end
+    end
+  end
+
+  # Class for decal layer in a project file
+  class ProjectLayerDecal
+    attr_reader :definition, :name, :grid_size, :export_id, :folder, :include_image_sequence, :scaleable, :rotatable,
+                :values
+
+    # @param data_raw [Hash] raw data of layer
+    def initialize(data_raw)
+      @definition = data_raw['definition']
+      @name = data_raw['name']
+      @grid_size = [data_raw['gridSize']['x'], data_raw['gridSize']['y']]
+      @export_id = data_raw['exportID']
+      @folder = data_raw['folder']
+      @include_image_sequence = data_raw['includeImageSequence']
+      @scaleable = data_raw['scaleable']
+      @rotatable = data_raw['rotatable']
+      @values = data_raw['values']
+    end
+  end
+
   require_relative 'project_entity'
   require_relative 'project_tileset'
   require_relative 'level'
